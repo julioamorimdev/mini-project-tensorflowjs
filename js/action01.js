2e00dbb07b539efe7f0a7333927f3da54364f57e
System Administrator <root@MacBooks-MacBook-Pro.local>
2023-03-09 23:24:10 -0300

    atualização 1

--- js/action01.js
+++ js/action01.js
@@ -3,50 +3,77 @@
 function executar() {
 	let txt = '';
 
-	const fill0 = tf.fill([1], 0);
-	const fill1 = tf.fill([2, 2], 1);
-	const fill2 = tf.fill([2, 2], 2);
+	const tensor1d = tf.tensor([1.5]);
+	const asScalar = tensor1d.asScalar();
 
-	const zeros1 = tf.zeros([1]);
-	const zeros2 = tf.zeros([2, 2]);
-	const zeros3 = tf.zeros([2, 4]);
+	const tensor2d = tf.tensor([1, 2, 3, 4], [2, 2]);
+	const flatten = tensor2d.flatten();
 
-	const ones1 = tf.ones([1]);
-	const ones2 = tf.ones([2, 2]);
-	const ones3 = tf.ones([2, 4]);
+	const tensor1 = tf.tensor([1, 2, 3, 4], [2, 2]);
+	const as1D = tensor1.as1D();
 
-	const linspace1 = tf.linspace(0, 9, 10);
-	const linspace2 = tf.linspace(1, 10, 10);
-	const linspace3 = tf.linspace(1, 15, 15);
+	const tensor2 = tf.tensor([1, 2, 3, 4], [2, 2, 1]);
+	const as2D = tensor2.as2D(2, 2);
 
-	const range1 = tf.range(1, 11, 1);
-	const range2 = tf.range(0, 21, 2);
-	const range3 = tf.range(0, 101, 10);
+	const tensor3 = tf.tensor([1, 2, 3, 4], [1, 2, 2, 1]);
+	const as3D = tensor2.as3D(2, 2, 1);
 
-	txt += 'fill:\n\n';
-	txt += fill0.toString() + '\n\n';
-	txt += fill1.toString() + '\n\n';
-	txt += fill2.toString() + '\n\n';
+	const tensor4 = tf.tensor([1, 2, 3, 4]);
+	const as4D = tensor4.as4D(1, 2, 2, 1);
 
-	txt += 'zeros:\n\n';
-	txt += zeros1.toString() + '\n\n';
-	txt += zeros2.toString() + '\n\n';
-	txt += zeros3.toString() + '\n\n';
+	const tensor5 = tf.tensor([1, 2, 3, 4, 5, 6, 7, 8]);
+	const as5D = tensor5.as5D(1, 2, 2, 2, 1);
 
-	txt += 'ones:\n\n';
-	txt += ones1.toString() + '\n\n';
-	txt += ones2.toString() + '\n\n';
-	txt += ones3.toString() + '\n\n';
+	const tensor6 = tf.tensor([1.5, 2.7, 3.1, 4.8, 5.2, 6.9, 7.3, 8.7]);
+	const asType = tensor6.asType('int32');
 
-	txt += 'linspace:\n\n';
-	txt += linspace1.toString() + '\n\n';
-	txt += linspace2.toString() + '\n\n';
-	txt += linspace3.toString() + '\n\n';
+	txt += 'asScalar:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor1d.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += asScalar.toString() + '\n\n';
 
-	txt += 'range:\n\n';
-	txt += range1.toString() + '\n\n';
-	txt += range2.toString() + '\n\n';
-	txt += range3.toString() + '\n\n';
+	txt += 'flatten:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor2d.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += flatten.toString() + '\n\n';
+
+	txt += 'as1D:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor1.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += as1D.toString() + '\n\n';
+
+	txt += 'as2D:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor2.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += as2D.toString() + '\n\n';
+
+	txt += 'as3D:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor3.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += as3D.toString() + '\n\n';
+
+	txt += 'as4D:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor4.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += as4D.toString() + '\n\n';
+
+	txt += 'as5D:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor5.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += as5D.toString() + '\n\n';
+
+	txt += 'asType:\n\n';
+	txt += 'ANTES:\n';
+	txt += tensor6.toString() + '\n\n';
+	txt += 'DEPOIS:\n';
+	txt += asType.toString() + '\n\n';
 
 	exibir(txt);
 }
